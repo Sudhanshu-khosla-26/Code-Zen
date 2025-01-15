085e809a108e46270eee9739d889a78d0c9ddcf8
Rahmanhusain <rahmanhusain899@gmail.com>
2025-01-15 12:39:59 +0530

    Loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,82 +1,121 @@
-import './App.css'
-import { BrowserRouter } from "react-router-dom"
-import About from './components/About'
+import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+/* import About from './components/About'
 import Frontmen from './components/Frontmen';
 import HomePage from './components/HomePage'
 import Navbar from './components/Navbar';
 import Prizes from './components/Priczes';
 import Sponsors from './components/Sponsors';
 import Track from './components/Track'
-import Faqs from './components/Faqs'
-import { Analytics } from '@vercel/analytics/react';
-import Footer from './components/Footer';
-// import SvgAnimation from './components/SquidAnim';
-import Audio from './components/Audio';
-// import { useEffect } from 'react';
+import Faqs from './components/Faqs' */
+import { Analytics } from "@vercel/analytics/react";
+import Footer from "./components/Footer";
+import SvgAnimation from "./components/SquidAnim";
+import Audio from "./components/Audio";
+import { useState, useEffect, Suspense, lazy } from "react";
+import Loader from "./components/Loader";
+const About = lazy(() => import("./components/About"));
+const Frontmen = lazy(() => import("./components/Frontmen"));
+const HomePage = lazy(() => import("./components/HomePage"));
+const Navbar = lazy(() => import("./components/Navbar"));
+const Prizes = lazy(() => import("./components/Priczes"));
+const Sponsors = lazy(() => import("./components/Sponsors"));
+const Track = lazy(() => import("./components/Track"));
+const Faqs = lazy(() => import("./components/Faqs"));
 
 function App() {
-  // const [animationCompleted, setAnimationCompleted] = useState(true);
+  const [animationCompleted, setAnimationCompleted] = useState(false);
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setAnimationCompleted(true);
+    }, 1500); // Adjust the timeout duration to match the length of your animation
 
-  // useEffect(() => {
-  // const timer = setTimeout(() => {
-  //   setAnimationCompleted(true);
-  // }, 16000); // Adjust the timeout duration to match the length of your animation
-
-  // return () => clearTimeout(timer);
-  // }, []);
-
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
-    <BrowserRouter>
-      {/* {
+    <>
+      {/*  {
         !animationCompleted &&
-        // <SvgAnimation />
+        <SvgAnimation />
       } */}
-      <Navbar />
-      <Audio />
-      <div className={` overflow-x-hidden overflow-y-hidden  relative transition-opacity duration-1000 ease-in-out`}>
-        <div className="relative">
-          <HomePage />
-          <div className="absolute  bottom-0 left-0 w-full h-24 bg-gradient-to-t from-black to-transparent pointer-events-none"></div>
-        </div>
-        <div className="relative">
-          <About />
-          <div className="absolute bottom-0 left-0 w-full h-24 bg-gradient-to-t from-black to-transparent pointer-events-none"></div>
-          <div className="absolute top-0 left-0 w-full h-14 bg-gradient-to-b from-black to-transparent pointer-events-none"></div>
-        </div>
-        <div className="relative">
-          <Track />
-          <div className="absolute bottom-0 left-0 w-full h-16 bg-gradient-to-t from-black to-transparent pointer-events-none"></div>
-          <div className="absolute top-0 left-0 w-full h-14 bg-gradient-to-b from-black to-transparent pointer-events-none"></div>
-        </div>
-        <div className="relative">
-          <Sponsors />
-          <div className="absolute bottom-0 left-0 w-full h-24 bg-gradient-to-t from-black to-transparent pointer-events-none"></div>
-          <div className="absolute top-0 left-0 w-full h-14 bg-gradient-to-b from-black to-transparent pointer-events-none"></div>
-        </div>
-        <div className="relative">
-          <Prizes />
-          <div className="absolute bottom-0 left-0 w-full h-24 bg-gradient-to-t from-black to-transparent pointer-events-none"></div>
-          <div className="absolute top-0 left-0 w-full h-14 bg-gradient-to-b from-black to-transparent pointer-events-none"></div>
-        </div>
-        <div className="relative">
-          <Frontmen />
-          <div className="absolute bottom-0 left-0 w-full h-24 bg-gradient-to-t from-black to-transparent pointer-events-none"></div>
-          <div className="absolute top-0 left-0 w-full h-14 bg-gradient-to-b from-black to-transparent pointer-events-none"></div>
-        </div>
-        <div className="relative">
-          <Faqs />
-          <div className="absolute bottom-0 left-0 w-full h-20 bg-gradient-to-t from-[#110202] to-transparent pointer-events-none"></div>
-          <div className="absolute top-0 left-0 w-full h-14 bg-gradient-to-b from-black to-transparent pointer-events-none"></div>
-        </div>
+      {animationCompleted && <Navbar />}
+      <Audio className={`${animationCompleted === false ? "hidden" : ""}`} />
+      <div
+        className={`overflow-x-hidden overflow-y-hidden  relative transition-opacity duration-1000 ease-in-out`}
+      >
+        {!animationCompleted && (
+          <div className="z-30 flex flex-col fixed gap-5 items-center justify-center bg-black h-screen w-screen px-4 sm:px-6 md:px-8 lg:px-10 xl:px-12 2xl:px-16">
+            <div>
+              <Loader />
+            </div>
+            <div className="w-32 max-w-full">
+              <div className="h-0.5 w-full bg-pink-100 overflow-hidden">
+                <div className="progress w-full h-full bg-pink-500 left-right"></div>
+              </div>
+            </div>
+          </div>
+        )}
+        {animationCompleted && (
+        <Suspense
+          fallback={
+            <div className="z-40 flex flex-col fixed gap-5 items-center justify-center bg-black h-screen w-screen px-4 sm:px-6 md:px-8 lg:px-10 xl:px-12 2xl:px-16">
+              <div>
+                <Loader />
+              </div>
+              <div className="w-32 max-w-full">
+                <div className="h-0.5 w-full bg-pink-100 overflow-hidden">
+                  <div className="progress w-full h-full bg-pink-500 left-right"></div>
+                </div>
+              </div>
+            </div>
+          }
+        >
+          <div className="relative landAnim">
+            <HomePage />
+            <div className="absolute bottom-0 left-0 w-full h-24 bg-gradient-to-t from-black to-transparent pointer-events-none"></div>
+          </div>
+          <div className="relative">
+            <About />
+            <div className="absolute bottom-0 left-0 w-full h-24 bg-gradient-to-t from-black to-transparent pointer-events-none"></div>
+            <div className="absolute top-0 left-0 w-full h-14 bg-gradient-to-b from-black to-transparent pointer-events-none"></div>
+          </div>
+          <div className="relative">
+            <Track />
+            <div className="absolute bottom-0 left-0 w-full h-16 bg-gradient-to-t from-black to-transparent pointer-events-none"></div>
+            <div className="absolute top-0 left-0 w-full h-14 bg-gradient-to-b from-black to-transparent pointer-events-none"></div>
+          </div>
+          <div className="relative">
+            <Sponsors />
+            <div className="absolute bottom-0 left-0 w-full h-24 bg-gradient-to-t from-black to-transparent pointer-events-none"></div>
+            <div className="absolute top-0 left-0 w-full h-14 bg-gradient-to-b from-black to-transparent pointer-events-none"></div>
+          </div>
+          <div className="relative">
+            <Prizes />
+            <div className="absolute bottom-0 left-0 w-full h-24 bg-gradient-to-t from-black to-transparent pointer-events-none"></div>
+            <div className="absolute top-0 left-0 w-full h-14 bg-gradient-to-b from-black to-transparent pointer-events-none"></div>
+          </div>
+          <div className="relative">
+            <Frontmen />
+            <div className="absolute bottom-0 left-0 w-full h-24 bg-gradient-to-t from-black to-transparent pointer-events-none"></div>
+            <div className="absolute top-0 left-0 w-full h-14 bg-gradient-to-b from-black to-transparent pointer-events-none"></div>
+          </div>
+          <div className="relative">
+            <Faqs />
+            <div className="absolute bottom-0 left-0 w-full h-20 bg-gradient-to-t from-[#110202] to-transparent pointer-events-none"></div>
+            <div className="absolute top-0 left-0 w-full h-14 bg-gradient-to-b from-black to-transparent pointer-events-none"></div>
+          </div>
+        </Suspense>
+        )
+        }
         <div className="relative">
           <Footer />
         </div>
       </div>
 
       <Analytics />
-    </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/components/Loader.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Loader = () => {
+  return (
+    <>
+      {/* Circle Loader */}
+      <div className="loader flex">
+        <svg viewBox="0 0 80 80">
+          <circle id="test" cx="40" cy="40" r="32"></circle>
+        </svg>
+      </div>
+
+      {/* Triangle Loader */}
+      <div className="loader triangle">
+        <svg viewBox="0 0 86 80">
+          <polygon points="43 8 79 72 7 72"></polygon>
+        </svg>
+      </div>
+
+      {/* Rectangle Loader */}
+      <div className="loader">
+        <svg viewBox="0 0 80 80">
+          <rect x="8" y="8" width="64" height="64"></rect>
+        </svg>
+      </div>
+    </>
+  );
+};
+
+export default Loader;
